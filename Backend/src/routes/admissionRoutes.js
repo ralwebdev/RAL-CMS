2e033b2aafaabb818192c5998bbbc2a3690b2f34
ralwebdev <rupak@redapplelearning.in>
2026-04-23 16:05:15 +0530

    fix: getAdmissionById is not defined

--- Backend/src/routes/admissionRoutes.js
+++ Backend/src/routes/admissionRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { getAdmissions, createAdmission, updateAdmission } from '../controllers/admissionController.js';
+import { getAdmissions, getAdmissionById, createAdmission, updateAdmission } from '../controllers/admissionController.js';
 import { protect } from '../middleware/authMiddleware.js';
 
 const router = express.Router();
